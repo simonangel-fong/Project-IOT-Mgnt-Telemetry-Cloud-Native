a3d853ec58070f769780b1808caafe036ddb2600
simonangel-fong <simonangelfong@gmail.com>
2025-12-16 00:47:43 -0500

    test cicd

--- aws/baseline/output.tf
+++ aws/baseline/output.tf
@@ -8,7 +8,13 @@ output "ecs_cluster_name" {
 
 output "flyway_init_cmd" {
   value = <<EOF
-aws ecs run-task --launch-type "FARGATE" --region "${var.aws_region}" --cluster "${aws_ecs_cluster.ecs_cluster.name}" --task-definition "${aws_ecs_task_definition.flyway.arn}" --network-configuration "awsvpcConfiguration={subnets=[${join(",", [for s in aws_subnet.private : s.id])}],securityGroups=[${aws_security_group.flyway.id}]}" --output text
+  aws ecs run-task \ 
+    --launch-type "FARGATE" \
+    --region "${var.aws_region}" \
+    --cluster "${aws_ecs_cluster.ecs_cluster.name}" \
+    --task-definition "${aws_ecs_task_definition.flyway.arn}" \
+    --network-configuration "awsvpcConfiguration={subnets=[${join(",", [for s in aws_subnet.private : s.id])}],securityGroups=[${aws_security_group.flyway.id}]}" \
+    --output text
 EOF
 
 }
