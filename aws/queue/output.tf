e69e8b9bede099c823a64fa78fda118893863fb3
simonangel-fong <simonangelfong@gmail.com>
2025-12-16 20:44:53 -0500

    test cicd

--- aws/queue/output.tf
+++ aws/queue/output.tf
@@ -23,12 +23,13 @@ EOF
 output "kafka_init_cmd" {
   value = <<EOF
 
-  aws ecs run-task
-    --launch-type "FARGATE"
-    --region "${var.aws_region}"
-    --cluster ${aws_ecs_cluster.ecs_cluster.name}
-    --task-definition ${aws_ecs_task_definition.kafka_init.arn} 
-    --network-configuration "awsvpcConfiguration={subnets=[${join(",", [for s in aws_subnet.private : s.id])}],securityGroups=[${aws_security_group.kafka_init.id}]}"
-
+  aws ecs run-task    \
+    --launch-type "FARGATE"   \
+    --region "${var.aws_region}"    \
+    --cluster ${aws_ecs_cluster.ecs_cluster.name}   \
+    --task-definition ${aws_ecs_task_definition.kafka_init.arn}   \
+    --network-configuration "awsvpcConfiguration={subnets=[${join(",", [for s in aws_subnet.private : s.id])}],securityGroups=[${aws_security_group.kafka_init.id}]}"   \
+    --output json
+    
 EOF
 }
